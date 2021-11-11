ddbeeac33957fed241a7aceada9c3cc660352b8f
Nageswar <Nageswar.Sahoo@maersk.com>
2021-11-11 21:19:08 +0530

    model updated

--- S7/model/model.py
+++ S7/model/model.py
@@ -12,36 +12,36 @@ class CIFRModel(BaseModel):
         self.depthwise_separable_conv1 = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, groups=3, padding=0, bias=False),
             nn.BatchNorm2d(3),
-            nn.Dropout(.10),
+            nn.Dropout(.05),
             nn.ReLU(),
             nn.Conv2d(in_channels=3, out_channels=512, kernel_size=1, bias=False),
             nn.BatchNorm2d(512),
-            nn.Dropout(.10),
+            nn.Dropout(.05),
             nn.ReLU(),
         )  # output_size = 28 receptive field  : 5
 
         # Input Block
         self.depthwise_separable_conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, groups=64, padding=0, bias=False),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, groups=512, padding=0, bias=False),
             nn.BatchNorm2d(512),
-            nn.Dropout(.10),
+            nn.Dropout(.05),
             nn.ReLU(),
             nn.Conv2d(in_channels=512, out_channels=28, kernel_size=1, bias=False),
             nn.BatchNorm2d(28),
-            nn.Dropout(.10),
+            nn.Dropout(.05),
             nn.ReLU(),
         )  # output_size = 28 receptive field  : 5
         # Input Block
 
         self.convblock3 = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=28, out_channels=128, kernel_size=(3, 3), padding=0, bias=False, dilation=8),
+            nn.ConvTranspose2d(in_channels=28, out_channels=220, kernel_size=(3, 3), padding=0, bias=False, dilation=4),
+            nn.BatchNorm2d(220),
+            nn.Dropout(.05),
             nn.ReLU(),
-            nn.Dropout(.10),
-            nn.BatchNorm2d(128),
         )  # output_size = 26 receptive field  : 7
 
         self.convblock4 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=10, kernel_size=(3, 3), padding=0, bias=False, dilation=15),
+            nn.Conv2d(in_channels=220, out_channels=10, kernel_size=(3, 3), padding=0, bias=False, dilation=12),
             nn.ReLU()
         )  # output_size = 20 receptive field  : 12
 
