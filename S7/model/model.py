c95cca1d9f130ead5816ef01c9bc878e87560d8b
Nageswar <Nageswar.Sahoo@maersk.com>
2021-11-11 22:09:08 +0530

    model updated

--- S7/model/model.py
+++ S7/model/model.py
@@ -5,6 +5,7 @@ from normalization import norm
 
 
 class CIFRModel(BaseModel):
+
     def __init__(self, num_classes=10, normalizationtype='Layer', dropout=.01):
         super().__init__()
         # Input Block
@@ -12,11 +13,9 @@ class CIFRModel(BaseModel):
         self.depthwise_separable_conv1 = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, groups=3, padding=0, bias=False),
             nn.BatchNorm2d(3),
-            nn.Dropout(.05),
             nn.ReLU(),
             nn.Conv2d(in_channels=3, out_channels=512, kernel_size=1, bias=False),
             nn.BatchNorm2d(512),
-            nn.Dropout(.05),
             nn.ReLU(),
         )  # output_size = 28 receptive field  : 5
 
@@ -24,21 +23,17 @@ class CIFRModel(BaseModel):
         self.depthwise_separable_conv2 = nn.Sequential(
             nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, groups=64, padding=0, bias=False),
             nn.BatchNorm2d(512),
-            nn.Dropout(.05),
             nn.ReLU(),
             nn.Conv2d(in_channels=512, out_channels=28, kernel_size=1, bias=False),
             nn.BatchNorm2d(28),
-            nn.Dropout(.05),
             nn.ReLU(),
         )  # output_size = 28 receptive field  : 5
         # Input Block
 
         self.convblock3 = nn.Sequential(
             nn.ConvTranspose2d(in_channels=28, out_channels=128, kernel_size=(3, 3), padding=0, bias=False, dilation=4),
-            nn.BatchNorm2d(128),
-            nn.Dropout(.05),
             nn.ReLU(),
-
+            nn.BatchNorm2d(128),
         )  # output_size = 26 receptive field  : 7
 
         self.convblock4 = nn.Sequential(
