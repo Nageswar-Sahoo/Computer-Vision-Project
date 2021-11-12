87c747fcd6b0acbc91c325c8e9d1ab25b09b8a7a
Nageswar <Nageswar.Sahoo@maersk.com>
2021-11-12 08:06:20 +0530

    transpose 2d removed

--- S7/model/loss.py
+++ S7/model/loss.py
@@ -3,8 +3,6 @@ import torch.nn as nn
 
 def crossentropyloss(output, target):
 
-    print("crossentropyloss")
-    print(output)
-    print(target)
+
     criterion = nn.CrossEntropyLoss()
     return criterion(output, target)
