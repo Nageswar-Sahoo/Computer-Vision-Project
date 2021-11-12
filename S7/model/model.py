0eb7aee6817f20e8856a3a4eca7ad9b8d11b699b
Nageswar <Nageswar.Sahoo@maersk.com>
2021-11-12 07:55:52 +0530

    transpose 2d removed

--- S7/model/model.py
+++ S7/model/model.py
@@ -10,34 +10,34 @@ class CIFRModel(BaseModel):
             # Input Block
 
             self.depthwise_separable_conv1 = nn.Sequential(
-                nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, groups=3, padding=0, bias=False),
+                nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, groups=3, padding=1, bias=False),
                 nn.BatchNorm2d(3),
-                nn.Dropout(.05),
+                #nn.Dropout(.05),
                 nn.ReLU(),
-                nn.Conv2d(in_channels=3, out_channels=512, kernel_size=1, bias=False),
-                nn.BatchNorm2d(512),
-                nn.Dropout(.05),
+                nn.Conv2d(in_channels=3, out_channels=128, kernel_size=1, bias=False),
+                nn.BatchNorm2d(128),
+                #nn.Dropout(.05),
                 nn.ReLU(),
             )  # output_size = 28 receptive field  : 5
 
             # Input Block
             self.depthwise_separable_conv2 = nn.Sequential(
-                nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, groups=512, padding=0, bias=False),
-                nn.BatchNorm2d(512),
-                nn.Dropout(.05),
+                nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, groups=128, padding=3, dilation=1,
+                          bias=False),
+                nn.BatchNorm2d(128),
+                #nn.Dropout(.05),
                 nn.ReLU(),
-                nn.Conv2d(in_channels=512, out_channels=32, kernel_size=1, bias=False),
+                nn.Conv2d(in_channels=128, out_channels=32, kernel_size=1, bias=False),
                 nn.BatchNorm2d(32),
-                nn.Dropout(.05),
+                #nn.Dropout(.05),
                 nn.ReLU(),
             )  # output_size = 28 receptive field  : 5
             # Input Block
 
             self.convblock3 = nn.Sequential(
-                nn.ConvTranspose2d(in_channels=32, out_channels=200, kernel_size=(3, 3), padding=0, bias=False,
-                                   dilation=4),
+                nn.Conv2d(in_channels=32, out_channels=200, kernel_size=(3, 3), padding=1, bias=False, dilation=1),
                 nn.BatchNorm2d(200),
-                nn.Dropout(.05),
+                #nn.Dropout(.05),
                 nn.ReLU(),
 
             )  # output_size = 26 receptive field  : 7
@@ -57,5 +57,5 @@ class CIFRModel(BaseModel):
             x = self.convblock3(x)
             x = self.convblock4(x)
             x = self.gap(x)
-            x = x.reshape(-1, 10 * 1 * 1)
+            # x = x.reshape(-1, 10 * 1 * 1)
             return F.log_softmax(x, dim=-1)
