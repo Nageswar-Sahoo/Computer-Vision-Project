c58aa73ee2013158943bd0e4c31ed5dd0216c85d
Nageswar <Nageswar.Sahoo@maersk.com>
2021-11-26 19:25:51 +0530

    custom resnet model added

--- template/model/customresnet.py
+++ template/model/customresnet.py
@@ -12,7 +12,7 @@ class ResidualBlock(nn.Module):
         self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
-        self.droput = nn.Dropout(0.20)
+        self.droput = nn.Dropout(0.05)
 
     def forward(self, x):
         residual = x
@@ -36,14 +36,14 @@ class ResNet(nn.Module):
         self.preplayer = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3, 3), stride=1, padding=1, bias=False),
             nn.BatchNorm2d(64),
-            nn.Dropout(0.20),
+            nn.Dropout(0.05),
             nn.ReLU()
         )
         self.conv_X1 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=1, padding=1, bias=False),
             nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(128),
-            nn.Dropout(0.20),
+            nn.Dropout(0.05),
             nn.ReLU()
         )
         self.layer1 = block(128, 128, 1)
@@ -51,14 +51,14 @@ class ResNet(nn.Module):
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), stride=1, padding=1, bias=False),
             nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(256),
-            nn.Dropout(0.20),
+            nn.Dropout(0.05),
             nn.ReLU()
         )
         self.conv_X2 = nn.Sequential(
             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), stride=1, padding=1, bias=False),
             nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(512),
-            nn.Dropout(0.20),
+            nn.Dropout(0.05),
             nn.ReLU()
         )
         self.layer3 = block(512, 512, 1)
