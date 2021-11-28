4117aeeaa378658c2e948ecc06e3608917f72ef9
Nageswar <Nageswar.Sahoo@maersk.com>
2021-11-28 12:35:21 +0530

    trsfm file updated

--- template/model/customresnet.py
+++ template/model/customresnet.py
@@ -10,14 +10,18 @@ class ResidualBlock(nn.Module):
         self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.1)
+
 
     def forward(self, x):
         residual = x
         out = self.conv1(x.clone())
         out = self.bn1(out)
+        out = self.dropout(out)
         out = self.relu(out)
         out = self.conv2(out)
         out = self.bn2(out)
+        out = self.dropout(out)
         out = residual + out
         out = self.relu(out)
         return out
@@ -36,6 +40,7 @@ class ResNet(nn.Module):
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=1, padding=1, bias=False),
             nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(128),
+            nn.Dropout(0.1),
             nn.ReLU()
         )
         self.layer1 = residualblock(128, 128, 1)
@@ -43,12 +48,14 @@ class ResNet(nn.Module):
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), stride=1, padding=1, bias=False),
             nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(256),
+            nn.Dropout(0.1),
             nn.ReLU()
         )
         self.conv_X2 = nn.Sequential(
             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), stride=1, padding=1, bias=False),
             nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(512),
+            nn.Dropout(0.1),
             nn.ReLU()
         )
         self.layer3 = residualblock(512, 512, 1)
@@ -56,16 +63,13 @@ class ResNet(nn.Module):
                                         nn.Flatten(),
                                         nn.Dropout(0.1),
                                         nn.Linear(512, num_classes))
-        self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
         out = self.preplayer(x)
         out = self.conv_X1(out)
         out = self.layer1(out)
         out = self.layer2(out)
-        out = self.dropout(out)
         out = self.conv_X2(out)
-        out = self.dropout(out)
         out = self.layer3(out)
         out = self.classifier(out)
         return out
